51ca83e722e81f8ca170efae7882450e59278d2b
miruky <h.miyoki@gmail.com>
2025-10-09 21:21:04 +0900

    石の漢数字をずれ案内で使えるよう公開

--- src/ui/stage.ts
+++ src/ui/stage.ts
@@ -5,7 +5,7 @@
 import type { Rule } from '../lib/csstext';
 import type { Rect } from '../lib/judge';
 
-const NUMERALS = ['一', '二', '三', '四', '五', '六', '七', '八', '九'];
+export const NUMERALS = ['一', '二', '三', '四', '五', '六', '七', '八', '九'];
 
 let counter = 0;
 
